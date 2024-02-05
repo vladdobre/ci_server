3ad6caa335a46e7f7de21a6441921a862e9e2220
vladdobre <29517124+vladdobre@users.noreply.github.com>
2024-02-05 20:13:41 +0100

    feat:  Implement the webhook listener #2
    Implemented the listener and parser that creates a map from the request payload.

--- src/ContinuousIntegrationServer.java
+++ src/ContinuousIntegrationServer.java
@@ -5,7 +5,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
  
 import java.io.IOException;
- 
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -28,13 +32,29 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         System.out.println(target);
 
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
+        System.out.println("Payload: ");
+        Map<String, Object> payload = request_to_map(request.getParameter("payload"));
+        for(String key : payload.keySet()){
+            System.out.println(payload.get(key));
+        }
 
         response.getWriter().println("CI job done");
     }
+
+
+    /**
+     * This function transforms a string in json format into a String,Object map.
+     * @param request - Request payload from webhook. It is in the JSON format (but json isn't selected in the Webhook options!!)
+     * @return A mapping String,Object of the json payload
+     */
+    private Map<String, Object> request_to_map(String request){
+
+        Gson gson = new Gson();
+        Type mapType = new TypeToken<Map<String, Object>>() {}.getType();
+        return gson.fromJson(request, mapType);
+    }
+
+
  
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
